a87cece1d1136b435314e60a46760d6548c781db
ryu <43063676+kojipy@users.noreply.github.com>
2025-01-12 21:30:04 +0900

    add: whisper model

--- src/compile_effect.py
+++ src/compile_effect.py
@@ -30,6 +30,32 @@ class T5(Model):
         self._model.generate(self._task)
 
 
+class Whisper(Model):
+    def __init__(self, compile=False):
+        super().__init__()
+        # load model and processor
+        processor = WhisperProcessor.from_pretrained("openai/whisper-tiny")
+        self._model = WhisperForConditionalGeneration.from_pretrained(
+            "openai/whisper-tiny"
+        )
+        self._model.config.forced_decoder_ids = None
+
+        if compile:
+            self._model = self.compile()
+
+        # load dummy dataset and read audio files
+        ds = load_dataset(
+            "hf-internal-testing/librispeech_asr_dummy", "clean", split="validation"
+        )
+        sample = ds[0]["audio"]
+        self._task = processor(
+            sample["array"], sampling_rate=sample["sampling_rate"], return_tensors="pt"
+        ).input_features
+
+    def run(self):
+        self._model.generate(self._task)
+
+
 def run(model):
     start_time = time.time()
     model.run()
