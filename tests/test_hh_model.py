9f0b114053a7f3eaf0718d698bb629f0839cc2a0
ryu <43063676+kojipy@users.noreply.github.com>
2025-01-12 14:36:13 +0900

    add test code

---
+++ tests/test_hh_model.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+
+from transformers import pipeline
+                                                                                                                                  
+
+def test_forward():
+    transcriber = pipeline(task="automatic-speech-recognition")
+    transcript = transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
+
